e8c56f4d2d62c235b4d2b22aad4b1e27f2ff7cdc
KimChanWoo <dolguso@naver.com>
2022-05-14 15:20:30 +0900

    Genre class 장르 별 포인트 관련 변수 추가

--- Genre.cpp
+++ Genre.cpp
@@ -2,7 +2,8 @@
 #include "Genre.h"
 #include <string>
 
-inline Genre::Genre(std::string genre_name, double default_fee, double additional_fee) : genre_name(genre_name), default_fee(default_fee), additional_fee(additional_fee) {};
+inline Genre::Genre(std::string genre_name, double default_fee, double additional_fee, int default_point, int additional_point, int default_day_for_point) :
+	genre_name(genre_name), default_fee(default_fee), additional_fee(additional_fee), default_point(default_point), additional_point(additional_point), default_day_for_point(default_day_for_point) {};
 
 inline std::string Genre::get_name() const { return genre_name; }
 
@@ -15,3 +16,15 @@ inline void Genre::set_default_fee(double new_default_fee) { default_fee = new_d
 inline double Genre::get_additional_fee() const { return additional_fee; }
 
 inline void Genre::set_additional_fee(double new_additional_fee) { additional_fee = new_additional_fee; }
+
+inline int Genre::get_defualt_point() const { return default_point; }
+
+inline int Genre::get_additional_point() const { return additional_point; }
+
+inline int Genre::get_default_day_for_point() const { return default_day_for_point; }
+
+inline void Genre::set_defualt_point(int new_default_point) { default_point = new_default_point; }
+
+inline void Genre::set_additional_point(int new_additional_point) { additional_point = new_additional_point; }
+
+inline void Genre::set_default_day_for_point(int new_default_day_for_point) { default_day_for_point = new_default_day_for_point; }
--- Genre.h
+++ Genre.h
@@ -9,14 +9,23 @@ private:
 	std::string genre_name;
 	double default_fee;
 	double additional_fee;
+	int default_point;
+	int additional_point;
+	int default_day_for_point;
 public:
-	Genre(std::string genre_name, double default_fee, double additional_fee);
+	Genre(std::string genre_name, double default_fee, double additional_fee, int default_point, int additional_point, int default_day_for_point);
 	std::string get_name() const;
 	void set_name(std::string input_name);
 	double get_default_fee() const;
 	double get_additional_fee() const;
 	void set_default_fee(double new_default_fee);
 	void set_additional_fee(double new_additional_fee);
+	int get_defualt_point() const;
+	int get_additional_point() const;
+	int get_default_day_for_point() const;
+	void set_defualt_point(int new_default_point);
+	void set_additional_point(int new_additional_point);
+	void set_default_day_for_point(int new_default_day_for_point);
 };
 
 #endif // MOVIE_HH

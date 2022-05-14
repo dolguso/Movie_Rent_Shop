7303249f361785ceb0a52cd8620e6594a0b60902
KimChanWoo <dolguso@naver.com>
2022-05-14 16:31:47 +0900

    Rental 시간 변수 추가

--- Rental.h
+++ Rental.h
@@ -1,6 +1,7 @@
 // Rental.hh
 #pragma once
 #include "Movie.h"
+#include <ctime>
 
 class Rental {
 public:
@@ -10,6 +11,8 @@ public:
 	const Movie& getMovie() const;
 	int get_point() const;
 	int get_fee() const;
+	time_t get_rent_time() const;
+	void set_rent_time(time_t input_rent_time);
 
 private:
 	Movie rentedMovie;

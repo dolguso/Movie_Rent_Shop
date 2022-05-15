9ed07295e3922a64f838c769e61af9b6a568b545
KimChanWoo <dolguso@naver.com>
2022-05-15 21:18:14 +0900

    ReplaceAll return value 대입 과정 추가

--- Customer.cpp
+++ Customer.cpp
@@ -23,15 +23,17 @@ std::string wtime(const time_t& t) {
 	std::tm tm;
 	localtime_s(&tm, &t);
 	std::wstringstream wss;
-	wss << std::put_time(&tm, L"%F %T");
-	return std::string(wss.str().begin(), wss.str().end());
+	std::string time = std::to_string(tm.tm_year + 1900) + "-" + std::to_string(tm.tm_mon + 1) + "-" + std::to_string(tm.tm_mday) + " " + std::to_string(tm.tm_hour) + ":" + std::to_string(tm.tm_min) + ":" + std::to_string(tm.tm_sec);
+	//wss << std::put_time(&tm, L"%F %T");
+	//return std::string(wss.str().begin(), wss.str().end());
+	return time;
 }
 
 std::string fill_receipt_line(Customer customer, std::vector<Rental> customer_rental_list, std::string line) {
 	std::string base_str = line;
-	ReplaceAll(base_str, "{customer_name}", customer.getName());
-	ReplaceAll(base_str, "{customer_rent_count}", std::to_string(customer_rental_list.size()));
-	ReplaceAll(base_str, "{customer_age}", std::to_string(customer.get_age()));
+	base_str = ReplaceAll(base_str, "{customer_name}", customer.getName());
+	base_str = ReplaceAll(base_str, "{customer_rent_count}", std::to_string(customer_rental_list.size()));
+	base_str = ReplaceAll(base_str, "{customer_age}", std::to_string(customer.get_age()));
 
 	std::vector< Rental >::iterator iter = customer_rental_list.begin();
 	std::vector< Rental >::iterator iter_end = customer_rental_list.end();
@@ -44,20 +46,21 @@ std::string fill_receipt_line(Customer customer, std::vector<Rental> customer_re
 		whole_fee += temp.get_fee();
 	}
 
-	ReplaceAll(base_str, "{customer_earn_point}", std::to_string(earn_point));
-	ReplaceAll(base_str, "{customer_point}", std::to_string(customer.get_point() - earn_point));
-	ReplaceAll(base_str, "{movie_rent_whole_fee}", std::to_string(whole_fee));
+	base_str = ReplaceAll(base_str, "{customer_earn_point}", std::to_string(earn_point));
+	base_str = ReplaceAll(base_str, "{customer_point}", std::to_string(customer.get_point()));
+	base_str = ReplaceAll(base_str, "{movie_rent_whole_fee}", std::to_string(whole_fee));
 
 	return base_str;
 }
 
 std::string fill_receipt_line_by_rental(Rental rental, std::string line) {
 	std::string base_str = line;
-	ReplaceAll(base_str, "{movie_title}", rental.getMovie().get_movie_title());
-	ReplaceAll(base_str, "{movie_genre}", rental.getMovie().get_genre_name());
-	ReplaceAll(base_str, "{movie_rent_start}", wtime(rental.get_rent_time()));
-	ReplaceAll(base_str, "{movie_rent_term}", std::to_string(rental.getDaysRented()));
-	ReplaceAll(base_str, "{movie_rent_fee}", std::to_string(rental.get_fee()));
+	base_str.erase(0, 1);
+	base_str = ReplaceAll(base_str, "{movie_title}", rental.getMovie().get_movie_title());
+	base_str = ReplaceAll(base_str, "{movie_genre}", rental.getMovie().get_genre_name());
+	base_str = ReplaceAll(base_str, "{movie_rent_start}", wtime(rental.get_rent_time()));
+	base_str = ReplaceAll(base_str, "{movie_rent_term}", std::to_string(rental.getDaysRented()));
+	base_str = ReplaceAll(base_str, "{movie_rent_fee}", std::to_string(rental.get_fee()));
 
 	return base_str;
 }
